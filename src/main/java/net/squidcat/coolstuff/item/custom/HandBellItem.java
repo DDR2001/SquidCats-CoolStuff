55ad40f7700fefae13ebfbc8260dce1bd479b347
DDR2001 <dee_monsters@hotmail.com>
2024-11-05 09:27:51 -0600

    Extras

---
+++ src/main/java/net/squidcat/coolstuff/item/custom/HandBellItem.java
@@ -0,0 +1,13 @@
+package net.squidcat.coolstuff.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class HandBellItem extends Item {
+    public HandBellItem (Properties properties) {
+        super(properties);
+    }
+
+
+
+
+}
